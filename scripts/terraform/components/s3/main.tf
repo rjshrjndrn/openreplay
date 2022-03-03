043500a23f526263bd07fca19175d491c5800942
rjshrjndrn <rjshrjndrn@gmail.com>
2022-03-03 17:40:14 +0100

    chore(terraform): creating policy for the public s3

--- scripts/terraform/components/s3/main.tf
+++ scripts/terraform/components/s3/main.tf
@@ -14,3 +14,20 @@ resource "aws_s3_bucket_cors_configuration" "cors" {
          expose_headers  = []
   }
 }
+
+resource "aws_s3_bucket_policy" "s3policy" {
+  # Create policy only on public bucket
+  count = var.bucket_acl == "public-read" ? 1 : 0
+  bucket = aws_s3_bucket.bucket.bucket
+  policy = jsonencode({
+        "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AddPerm",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${aws_s3_bucket.bucket.bucket}/*"
+        }
+    ]})
+}
